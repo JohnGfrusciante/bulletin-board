758480fee5e65f50c226b2e2a1e32d53e1ce5300
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-04-29 23:21:28 +0900

    メッセージ投稿機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :set_topic, only: [:create]
+
+  def create
+    @message = Message.new(message_params)
+    @message.save!
+    redirect_to "/topics/#{@topic.id}"
+  end
+
+
+  private
+  def set_topic
+    @topic = Topic.find(params[:topic_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, topic_id: @topic.id)
+  end
+
 end
