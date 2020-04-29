4c61e4348163dfebbb99b1fc80149fa32b8918d4
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-04-29 21:21:10 +0900

    スレッド(topic)作成機能の実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,4 +3,44 @@ class TopicsController < ApplicationController
   def index
   end
 
+  def new
+    @topic = Topic.new
+    @categories = Category.all
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    category_ids = params[:topic][:category_ids]
+    @message = params[:topic][:message]
+
+    if category_ids.present? and @message.present?
+      @topic.save!
+      
+      category_ids.each do |category_id|
+        @topic_category = TopicCategory.new(category_id: category_id, topic_id: @topic.id)
+      end
+      first_message_create
+
+    else
+      redirect_to action: :new
+    end
+
+  end
+
+
+  def first_message_create
+    @first_message = Message.new(text: @message, user_id: current_user.id, topic_id: @topic.id)
+    @first_message.save!
+  end
+
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title, category_ids: [])
+  end
+
+  # def message_params
+  #   params.require(:message).permit(:text).merge(user_id: current_user.id, topic_id: @topic.id)
+  # end
+
 end
